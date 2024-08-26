51f29f51d02f7bf23591a2583352b1a3ac87b733
Tito <154619686+Tilani1234@users.noreply.github.com>
2024-08-26 13:05:32 -0400

    initials.py

---
+++ initials.py
@@ -0,0 +1,9 @@
+print(" TTTTT    L")
+print("   T      L")
+print("   T      L")
+print("   T      L")
+print("   T      L")
+print("   T      L")
+print("   T      LLLLL")
+#Tito Laniyan
+#I liked coding 
